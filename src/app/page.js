680487cdd4dadbd26d615697c5509b635070af98
FarazTheRealOne <141612761+FarazTheRealOne@users.noreply.github.com>
2024-04-26 11:23:27 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -18,6 +18,7 @@ export default function Home() {
       <Features />
       <InfoSection/>
       <FeaturesSection />
+    <FeaturesSection />
       <InquiryForm />
       <Footer />
       {/* </Suspense> */}
